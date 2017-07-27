340a658d66cd412324da88a0a22b456ff4d24a62
Ayman <ayman.moussa14@imperial.ac.uk>
2017-07-27 17:05:24 +0100

    Problem 119 in C

--- Problem 119/119.c
+++ Problem 119/119.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#include <omp.h>
 
-long digit_power_sum(unsigned long n) {
+int digit_sum( long n) {
     int sum = 0;
-    int tmp = n;
-    while (tmp) {
-           sum += tmp % 10;
-           tmp /= 10;
+    while (n) {
+        sum += n % 10;
+        n /= 10;
     }
-    if (sum == 1) return 0;
-    // power method
-    int e = 2;
-    while (pow(sum,e) < n) {
-        e++;
-     //   printf("pow(%d,%d) = %d\n",sum,e,(int)pow(sum,e));
+    return sum;
+}
+
+int comp(const void *a,const void *b) {
+    if (*( long*)a - *( long*)b == 0) {
+        return 0;
+    } else if (*( long*)a - *( long*)b > 0) {
+        return 1;
+    } else {
+        return -1;
     }
-    return (long)pow(sum,e) == n;
 }
 
 int main() {
-   // digit_power_sum(512);
-    #pragma omp parallel for 
-    for (long i=205962976; i<9999999999; i++) {
-        if (digit_power_sum(i)) printf("%ld\t%ld\n",i,digit_power_sum(i));
-    }    
+    long elems[32];
+    int an = 0;
+    for (int x=2; x<70; x++) {
+        for (int n=2; n<20; n++) {
+            long r = pow(x,n);
+            if (digit_sum(r) == x && r>9) {
+                elems[an++] = r;  
+            }
+        }
+    }
+    qsort((void*)elems,32,sizeof(long),comp);
+    printf("\n%ld\n",elems[29]);
     return 0;
 }
